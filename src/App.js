77c5497026471cf93de74f9ad79ad049baeb38b2
Guldana Baktybekova <guldanabaktybekova10@gmail.com>
2022-06-16 14:05:44 +0600

    Add update

--- src/App.js
+++ src/App.js
@@ -1,20 +1,22 @@
-import { Routes, Route } from 'react-router-dom'
-import ArticleCreate from './components/ArticleCreate';
-import ArticleFull from './components/ArticleFull';
-import ArticleList from './components/ArticleList';
-import ArticleUpdate from './components/ArticleUpdate';
-import ArticleDelete from './components/ArticleDelete';
-function App() {
-  return <div className="App">
-    <Routes>
-      <Route path="/" element={<ArticleList/>}/>
-      <Route path="/create" element={<ArticleCreate/>}/>
-      <Route path="/:id" element={<ArticleFull/>}/>
-      <Route path="/update:id" element={<ArticleUpdate/>}/>
-      <Route path="/delete:id" element={<ArticleDelete/>}/>
-    </Routes>
+import { Route, Routes } from 'react-router-dom';
+import ArticleCreate from './ArticleCreate';
+import ArticleFull from './ArticleFull';
+import ArticleList from './ArticleList';
+import ArticleUpdate from './ArticleUpdate';
+import ArticleDelete from './ArticleDelete';
 
-  </div>;
+function App() {
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<ArticleList />} />
+        <Route path="/create" element={<ArticleCreate />} />
+        <Route path="/:id" element={<ArticleFull />} />
+        <Route path="/update/:id" element={<ArticleUpdate />} />
+        <Route path="/delete/:id" element={<ArticleDelete />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
